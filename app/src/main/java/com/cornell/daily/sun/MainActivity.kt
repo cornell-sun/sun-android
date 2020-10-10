3004d4a035f9ae90d05251c79ba6ffb517fd8434
Mike Fang <mfang2197@gmail.com>
2020-10-10 17:01:27 -0400

    added navigation

--- app/src/main/java/com/cornell/daily/sun/MainActivity.kt
+++ app/src/main/java/com/cornell/daily/sun/MainActivity.kt
@@ -2,10 +2,15 @@ package com.cornell.daily.sun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_feed)
+        setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+        val navController = navHostFragment.navController
+        navController.navigate(R.id.main_feed_fragment)
     }
 }
---
+++ app/src/main/java/com/cornell/daily/sun/MainFeedFragment.kt
@@ -0,0 +1,18 @@
+package com.cornell.daily.sun
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFeedFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main_feed, container, false)
+    }
+}
