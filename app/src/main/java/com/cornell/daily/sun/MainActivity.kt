bcec0326c01bd930aaa9c4a04922a2d3bcbad858
Mike Fang <mfang2197@gmail.com>
2021-01-14 17:12:53 -0800

    added toolbar

--- app/src/main/java/com/cornell/daily/sun/MainActivity.kt
+++ app/src/main/java/com/cornell/daily/sun/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cornell.daily.sun
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,6 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         val navController = navHostFragment.navController
