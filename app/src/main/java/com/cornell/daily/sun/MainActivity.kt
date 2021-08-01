adf2ee897a29c7e990bd067934b56b9394a28c14
Mike Fang <mf647@cornell.edu>
2021-08-01 09:25:19 -0700

    Added search functionality (#9)
    * search almost complete
    
    * search view now has clickable articles
    
    * Finished search functionality

--- app/src/main/java/com/cornell/daily/sun/MainActivity.kt
+++ app/src/main/java/com/cornell/daily/sun/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.cornell.daily.sun
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
+import com.cornell.daily.sun.util.InjectorUtils
+import com.cornell.daily.sun.viewmodels.SearchViewModel
 import com.google.android.gms.ads.MobileAds
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val searchViewModel: SearchViewModel by viewModels { InjectorUtils.provideViewModelFactory() }
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var appHeaderView: TextView
+    private lateinit var searchButtonView: ImageView
+    private lateinit var searchBox: EditText
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MobileAds.initialize(applicationContext)
@@ -22,10 +41,74 @@ class MainActivity : AppCompatActivity() {
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        // Set up Search Input Box here since Search is anticipated to appear in many screens
+        searchBox = findViewById(R.id.search_box)
+        searchBox.visibility = View.GONE
+        searchBox.setOnEditorActionListener { v, actionId, event ->
+            return@setOnEditorActionListener when (actionId) {
+                EditorInfo.IME_ACTION_SEND -> {
+                    searchViewModel.setQuery(v.text.toString())
+                    closeSearchSoftKeyboard()
+                    true
+                }
+                else -> false
+            }
+        }
+
+        appHeaderView = findViewById(R.id.app_header_title)
+
+        bottomNavigationView = findViewById(R.id.bottom_nav_view)
         bottomNavigationView.setupWithNavController(navController)
         bottomNavigationView.itemIconTintList = null
+        showBottomNavigationBar()
+
+        searchButtonView = findViewById(R.id.app_header_search)
+        searchButtonView.setOnClickListener { navController.navigate(R.id.main_feed_to_search) }
         navController.navigate(R.id.main_feed_fragment)
     }
 
+    fun setupHeader(fontPath: String, text: String, textSize: Int) {
+        appHeaderView.visibility = View.VISIBLE
+        appHeaderView.typeface = Typeface.createFromAsset(
+            assets,
+            fontPath
+        )
+        appHeaderView.textSize = resources.getDimension(textSize)
+        appHeaderView.text = text
+    }
+
+    fun hideHeaderText() {
+        appHeaderView.visibility = View.GONE
+    }
+
+    fun hideSearchButton() {
+        searchButtonView.visibility = View.GONE
+    }
+
+    fun showSearchButton() {
+        searchButtonView.visibility = View.VISIBLE
+    }
+
+    fun showSearchBox() {
+        searchBox.visibility = View.VISIBLE
+    }
+
+    fun hideSearchBox() {
+        searchBox.visibility = View.GONE
+    }
+
+    fun hideBottomNavigationBar() {
+        bottomNavigationView.visibility = View.GONE
+    }
+
+    fun showBottomNavigationBar() {
+        bottomNavigationView.visibility = View.VISIBLE
+    }
+
+    fun closeSearchSoftKeyboard() {
+        val imm = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchBox.windowToken, 0)
+        searchBox.clearFocus()
+    }
 }
---
+++ app/src/main/java/com/cornell/daily/sun/SearchFragment.kt
@@ -0,0 +1,97 @@
+package com.cornell.daily.sun
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cornell.daily.sun.adapters.CompactPostsAdapter
+import com.cornell.daily.sun.util.InjectorUtils
+import com.cornell.daily.sun.viewmodels.PostViewModel
+import com.cornell.daily.sun.viewmodels.SearchViewModel
+import kotlinx.android.synthetic.main.feed_fragment.view.*
+import kotlinx.android.synthetic.main.main_feed_fragment.view.*
+import kotlinx.android.synthetic.main.search_fragment.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class SearchFragment : Fragment() {
+
+    private val postViewModel: PostViewModel by activityViewModels { InjectorUtils.provideViewModelFactory() }
+    private val searchViewModel: SearchViewModel by activityViewModels { InjectorUtils.provideViewModelFactory() }
+
+    private lateinit var searchContentRecyclerView: RecyclerView
+
+    private lateinit var searchContentAdapter: CompactPostsAdapter
+
+    private lateinit var searchContentLayoutManager: LinearLayoutManager
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = inflater.inflate(R.layout.search_fragment, container, false)
+        setHasOptionsMenu(true)
+        searchContentRecyclerView = binding.search_view_content
+        searchContentAdapter = CompactPostsAdapter(activity, postViewModel::pushPost)
+        searchContentLayoutManager = LinearLayoutManager(activity)
+        searchContentRecyclerView.apply {
+            adapter = searchContentAdapter
+            layoutManager = searchContentLayoutManager
+        }
+
+
+        postViewModel.postStack.observe(viewLifecycleOwner) {
+            if (!it.isEmpty()) {
+                findNavController().navigate(R.id.search_to_post)
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            searchViewModel.flow.collectLatest { pagingData ->
+                searchContentAdapter.submitData(pagingData)
+            }
+        }
+
+        searchViewModel.query.observe(viewLifecycleOwner) {
+            if (it != null) {
+                GlobalScope.launch {
+                    searchContentAdapter.refresh()
+                    binding.search_swipe_container.isRefreshing = false
+                }
+            }
+        }
+
+        binding.search_swipe_container.setOnRefreshListener {
+            GlobalScope.launch {
+                searchContentAdapter.refresh()
+                binding.search_swipe_container.isRefreshing = false
+            }
+        }
+
+        (activity as MainActivity).hideHeaderText()
+        (activity as MainActivity).hideSearchButton()
+        (activity as MainActivity).showSearchBox()
+        (activity as MainActivity).hideBottomNavigationBar()
+        (activity as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        return binding
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        findNavController().navigate(R.id.main_feed_fragment)
+        (activity as MainActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
+        (activity as MainActivity).closeSearchSoftKeyboard()
+        return super.onOptionsItemSelected(item)
+    }
+}
